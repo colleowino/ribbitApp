f368dc7af0e956c0819edb4e2a5af772324a658a
colleowino <colleowino@gmail.com>
2015-04-24 19:22:16 +0300

    users are now logged in upon creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 	  @user = User.new(params[:user])
 	 
 	  if @user.save
+	  	session[:user_id] = @user.id
 	    redirect_to @user, notice: "Thank you for signing up for Ribbit!"
 	  else
 	    render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  post 'sessions/create'
+  get 'sessions/destroy'
+
   resources :users
   root to: 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
