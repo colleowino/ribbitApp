3cc4bc9b8613ebd9a39516a21d772d312546e62e
Kole <colleowino@gmail.com>
2015-12-11 23:16:51 +0300

    Update ribbit.rb

--- app/models/ribbit.rb
+++ app/models/ribbit.rb
@@ -2,6 +2,7 @@ class Ribbit < ActiveRecord::Base
 	default_scope { order(created_at: :desc)  }
 	attr_accessible :content, :userid
 	belongs_to :user
+	has_many: ribbits
 	
 	validates :content, length: { maximum: 140 }
 end
