4d87368668413b8fb1ed49304701db30ac14af97
Kole <colleowino@gmail.com>
2015-12-19 18:08:43 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
    Mayor.create(name: 'Emanuel', city: cities.first)
+
+  towns = Tow.create([{name: 'Nairobi'}, {name: 'Mombasa'}])
+  
