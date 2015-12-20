f9aebc50b22dd27ed3a5760bb346a6e680ae0fdb
Kole <colleowino@gmail.com>
2015-12-20 15:16:24 +0300

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all, :ribits
+  fixtures :all, :ribits, :posts
 
   # Add more helper methods to be used by all tests here...
 end
